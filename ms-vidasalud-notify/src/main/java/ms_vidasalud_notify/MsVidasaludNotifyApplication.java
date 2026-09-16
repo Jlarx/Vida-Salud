f6fbc9625ccd80f6be56cc495540443fa6014880
Jlarx <joh.laram@duocuc.cl>
2026-09-15 23:59:02 -0300

    /ms-vidasalud-notify (Spring Boot)

---
+++ ms-vidasalud-notify/src/main/java/ms_vidasalud_notify/MsVidasaludNotifyApplication.java
@@ -0,0 +1,13 @@
+package ms_vidasalud_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVidasaludNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVidasaludNotifyApplication.class, args);
+	}
+
+}
