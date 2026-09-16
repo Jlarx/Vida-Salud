b7cc2c5f4ddcb79ccf794331d150ed6c6d9c7d0c
Jlarx <joh.laram@duocuc.cl>
2026-09-15 23:59:02 -0300

    /ms-vidasalud-report (Spring Boot)

---
+++ ms-vidasalud-report/src/main/java/ms_vidasalud_report/MsVidasaludReportApplication.java
@@ -0,0 +1,13 @@
+package ms_vidasalud_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVidasaludReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVidasaludReportApplication.class, args);
+	}
+
+}
